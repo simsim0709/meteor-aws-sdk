e4f3fd69fedf9cb90e261a06cb8325ee06a7d9c8
Ilkwon Sim <sim@theloho.com>
2017-01-10 17:22:01 +0900

    Remove client files

--- package.js
+++ package.js
@@ -1,32 +1,26 @@
 Package.describe({
-  summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
+  summary: 'SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF',
   version: '2.4.9_1',
-  name: 'peerlibrary:aws-sdk',
-  git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
+  name: 'thelohoadmin:aws-sdk-server',
+  git: 'https://github.com/simsim0709/meteor-aws-sdk-server.git',
 });
 
 Npm.depends({
-  'aws-sdk': '2.4.9'
+  'aws-sdk': '2.4.9',
 });
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@1.0.3.2');
-  api.use(['peerlibrary:blocking@0.5.2', 'underscore']);
+  api.use(['peerlibrary:blocking@0.5.2', 'underscore'], 'server');
 
-  api.export('AWS');
+  api.export('AWS', 'server');
 
   api.add_files([
-    'server.js'
+    'server.js',
   ], 'server');
-
-  api.add_files([
-    'aws-sdk-js/dist/aws-sdk.js',
-    'client.js'
-  ], 'client');
 });
 
 Package.on_test(function (api) {
-  api.use(['peerlibrary:aws-sdk', 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('client-tests.js', 'client');
+  api.use(['thelohoadmin:aws-sdk-server', 'tinytest', 'test-helpers'], 'server');
   api.add_files('server-tests.js', 'server');
 });
